4c622f386500fd1f04b19e232934063c54edc5e7
Richard Barrington-Hill <bazzlad@gmail.com>
2025-08-13 14:16:48 +0200

    Laravel +aimeos

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', '\Aimeos\Shop\Controller\CatalogController@homeAction')->name('aimeos_home');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile/me', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/me', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/me', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
