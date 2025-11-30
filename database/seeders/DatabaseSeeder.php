1d3b338f459878e580331c0554eb72ebb9abc8b9
Arno Poot <arno@xzn.digital>
2025-11-30 11:09:10 +0100

    Set seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,30 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create admin user
-        User::factory()->admin()->create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('secret1234'),
-            'role' => UserRole::Admin,
-        ]);
-
-        // Create artist user
-        User::factory()->artist()->create([
-            'name' => 'Artist User',
-            'email' => 'artist@example.com',
-            'password' => bcrypt('secret1234'),
-            'role' => UserRole::Artist,
-        ]);
-
-        // Create regular user
-        User::factory()->user()->create([
-            'name' => 'Regular User',
-            'email' => 'user@example.com',
-            'password' => bcrypt('secret1234'),
-            'role' => UserRole::User,
-        ]);
-
         $this->call(ProductSeeder::class);
     }
 }
